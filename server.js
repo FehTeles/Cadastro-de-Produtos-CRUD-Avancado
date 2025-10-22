a26c04e816ee0081f21951216b303f3fe205ed67
Fërb <74201397+FehTeles@users.noreply.github.com>
2025-10-21 22:26:54 -0300

    Host on listen

--- server.js
+++ server.js
@@ -37,5 +37,7 @@ server.delete("/produtos/:id", async (request, reply) => {
 });
 
 // inicia o servidor
-server.listen({ port: process.env.PORT ?? 3000
+server.listen({ 
+  host: '0.0.0.0',
+  port: process.env.PORT ?? 3000
 });
